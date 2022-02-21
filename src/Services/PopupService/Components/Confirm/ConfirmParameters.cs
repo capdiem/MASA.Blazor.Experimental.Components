45f550caa7a3f914128f18a3555f5d4f43d848c3
capdiem <capdiem@live.com>
2022-02-21 19:13:02 +0800

    🆕 feat(confirm): ConfirmAsync support onOk

---
+++ src/Services/PopupService/Components/Confirm/ConfirmParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace MASA.Blazor.Experimental.Components;
+
+public class ConfirmParameters
+{
+    public Dictionary<string, object> Attributes { get; set; }
+
+    public Func<PopupOkEventArgs<bool>, Task>? OnOk { get; set; }
+
+    public Dictionary<string, object> ToDictionary(string? title, string? content)
+    {
+        return new Dictionary<string, object>()
+        {
+            { "Title", title },
+            { "Content", content },
+            { nameof(Attributes), Attributes },
+            { nameof(OnOk), OnOk }
+        };
+    }
+}
