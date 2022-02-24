bd133d0f7bf90f7ac62b7808ff7b3f46265ab88d
capdiem <capdiem@live.com>
2022-02-24 17:50:41 +0800

    ⬆ chore: Upgrade MASA.Blazor to 0.2.10649

--- src/_Imports.cs
+++ src/_Imports.cs
@@ -1,6 +1,7 @@
 ﻿// Global using directives
 
 global using BlazorComponent;
+global using Masa.Blazor;
 global using MASA.Blazor.Experimental.Components.Helpers;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Web;
